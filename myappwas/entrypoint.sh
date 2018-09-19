b35b395371b65612c1a02b082f81853dde853eab
eastuni <donghyuni@gmail.com>
2018-09-19 15:37:47 +0900

    libs

--- myappwas/entrypoint.sh
+++ myappwas/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-. ~/.bash_aliases
 
 run_was(){
 	APP_NAME=$1
@@ -17,6 +16,10 @@ startup_was(){
 	${WAS_HOME}/${APP_NAME}/startup.sh
 }
 
+service apache2 start
+
+su - apprun
+. /home/apprun/.bash_aliases
 startup_was online
 run_was admin
 
